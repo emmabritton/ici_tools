22ec3efa7629aabe975527d613e86e0fa9b71879
Emma Britton <emmabritton@pm.me>
2024-01-04 11:34:14 +0000

    Clippy

--- src/converter.rs
+++ src/converter.rs
@@ -1,11 +1,11 @@
-use std::collections::HashSet;
-use std::fs::File;
-use std::io::{BufReader, Cursor, Read, Write};
-use std::path::PathBuf;
 use clap::Parser;
 use color_eyre::eyre::eyre;
 use color_eyre::Result;
 use pixels_graphics_lib::prelude::*;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufReader, Cursor, Read, Write};
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about="Converts image files to ICIs", long_about = None)]
@@ -14,7 +14,7 @@ struct Args {
     input: PathBuf,
 
     #[arg(short, long, value_name = "FILE")]
-    output: Option<PathBuf>
+    output: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
@@ -42,9 +42,9 @@ fn process_input(file_path: PathBuf) -> Result<Image> {
         Some("bmp") => Ok(load_image(bytes, ImageFormat::Bmp)?),
         Some("tiff") => Ok(load_image(bytes, ImageFormat::Tiff)?),
         Some("tga") => Ok(load_image(bytes, ImageFormat::Tga)?),
-        Some("jpeg")|Some("jpg") => Ok(load_image(bytes, ImageFormat::Jpeg)?),
+        Some("jpeg") | Some("jpg") => Ok(load_image(bytes, ImageFormat::Jpeg)?),
         Some("webp") => Ok(load_image(bytes, ImageFormat::WebP)?),
-        Some(_) => Err(eyre!("Unsupported image file type"))
+        Some(_) => Err(eyre!("Unsupported image file type")),
     }
 }
 
@@ -54,31 +54,47 @@ fn convert(image: Image) -> Result<IndexedImage> {
     let colors: HashSet<Color> = HashSet::from_iter(image.pixels().iter().copied());
 
     if width >= 256 || height >= 256 {
-        return Err(eyre!("Image is too big, max width and height are 255 (was {width} x {height})"));
+        return Err(eyre!(
+            "Image is too big, max width and height are 255 (was {width} x {height})"
+        ));
     }
 
     if colors.len() >= 256 {
-        return Err(eyre!("Image has too many colours, max is 255 (was {})", colors.len()));
+        return Err(eyre!(
+            "Image has too many colours, max is 255 (was {})",
+            colors.len()
+        ));
     }
 
     let palette: Vec<IciColor> = colors.iter().map(|c| c.to_ici()).collect();
-    let pixels = image.pixels().iter().map(|p| palette.iter().position(|c| c.to_color() == *p).unwrap() as u8).collect();
+    let pixels = image
+        .pixels()
+        .iter()
+        .map(|p| palette.iter().position(|c| c.to_color() == *p).unwrap() as u8)
+        .collect();
 
-    Ok(IndexedImage::new(width as u8, height as u8, palette, pixels)?)
+    Ok(IndexedImage::new(
+        width as u8,
+        height as u8,
+        palette,
+        pixels,
+    )?)
 }
 
-fn create_file_name(input: &PathBuf, output_file: Option<PathBuf>) -> Result<PathBuf> {
+fn create_file_name(input: &Path, output_file: Option<PathBuf>) -> Result<PathBuf> {
     if let Some(path) = output_file {
         Ok(path)
     } else {
-        let input_file_name =input.file_stem();
+        let input_file_name = input.file_stem();
         let dir = input.parent();
         if let (Some(file_name), Some(dir)) = (input_file_name.and_then(|s| s.to_str()), dir) {
             let mut path = PathBuf::from(dir);
             path.push(format!("{file_name}.ici"));
             Ok(path)
         } else {
-            Err(eyre!("Couldn't create file name as input file is incomplete/invalid"))
+            Err(eyre!(
+                "Couldn't create file name as input file is incomplete/invalid"
+            ))
         }
     }
 }
--- src/viewer.rs
+++ src/viewer.rs
@@ -1,16 +1,16 @@
+use clap::Parser;
+use pixels_graphics_lib::buffer_graphics_lib::Graphics;
+use pixels_graphics_lib::prelude::{IndexedImage, KeyCode, Timing, WHITE};
+use pixels_graphics_lib::{run, Options, System};
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::PathBuf;
-use clap::Parser;
-use pixels_graphics_lib::prelude::{IndexedImage, KeyCode, Timing, WHITE};
-use pixels_graphics_lib::{Options, run, System};
-use pixels_graphics_lib::buffer_graphics_lib::Graphics;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "Displays ICI files", long_about = None)]
 struct Args {
     #[arg(value_name = "FILE", help = "Image to show")]
-    input: PathBuf
+    input: PathBuf,
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -18,14 +18,24 @@ fn main() -> color_eyre::Result<()> {
 
     let args = Args::parse();
 
-    let filename = args.input.file_stem().and_then(|s| s.to_str()).unwrap_or("Image");
+    let filename = args
+        .input
+        .file_stem()
+        .and_then(|s| s.to_str())
+        .unwrap_or("Image");
     let file = File::open(&args.input)?;
     let mut reader = BufReader::new(file);
     let mut bytes = vec![];
     reader.read_to_end(&mut bytes)?;
     let image = IndexedImage::from_file_contents(&bytes)?.0;
 
-    run(image.width() as usize, image.height() as usize, filename, ImageDisplayer::new(image), Options::default())?;
+    run(
+        image.width() as usize,
+        image.height() as usize,
+        filename,
+        ImageDisplayer::new(image),
+        Options::default(),
+    )?;
 
     Ok(())
 }
@@ -33,7 +43,7 @@ fn main() -> color_eyre::Result<()> {
 #[derive(Debug)]
 struct ImageDisplayer {
     image: IndexedImage,
-    exit: bool
+    exit: bool,
 }
 
 impl ImageDisplayer {
@@ -49,7 +59,7 @@ impl System for ImageDisplayer {
 
     fn render(&mut self, graphics: &mut Graphics) {
         graphics.clear(WHITE);
-        graphics.draw_indexed_image((0,0), &self.image);
+        graphics.draw_indexed_image((0, 0), &self.image);
     }
 
     fn on_key_down(&mut self, keys: Vec<KeyCode>) {
